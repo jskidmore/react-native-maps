001c65182fb93141b36f8d3886242a2bf5b4ec6d
jskidmore <jbskid@gmail.com>
2020-06-29 10:34:13 -0500

    fix

--- lib/ios/AirMaps/AIRMapLocalTileOverlay.m
+++ lib/ios/AirMaps/AIRMapLocalTileOverlay.m
@@ -54,7 +54,21 @@
         }
     };
 
-    [super loadTileAtPath:newPath result:completionHandler];
+    NSMutableString *tileFilePath = [self.URLTemplate mutableCopy];
+    [tileFilePath replaceOccurrencesOfString: @"{x}" withString:[NSString stringWithFormat:@"%li", (long)newPath.x] options:0 range:NSMakeRange(0, tileFilePath.length)];
+    [tileFilePath replaceOccurrencesOfString:@"{y}" withString:[NSString stringWithFormat:@"%li", (long)newPath.y] options:0 range:NSMakeRange(0, tileFilePath.length)];
+    [tileFilePath replaceOccurrencesOfString:@"{z}" withString:[NSString stringWithFormat:@"%li", (long)newPath.z] options:0 range:NSMakeRange(0, tileFilePath.length)];
+    if ([[NSFileManager defaultManager] fileExistsAtPath:tileFilePath]) {
+        NSData* tile = [NSData dataWithContentsOfFile:tileFilePath];
+
+        if (self.overzoomEnabled && path.z > self.overzoomThreshold) {
+            tile = [self overzoomImageDataForPath: path imageData: tile];
+        }
+
+        result(tile, nil);
+    } else {
+        result(nil, nil);
+    }
 }
 
 -(NSData *)overzoomImageDataForPath:(MKTileOverlayPath)path imageData:(NSData *)data
@@ -76,21 +90,6 @@
 
     return UIImagePNGRepresentation(image);
 }
-
-
-
-/*-(void)loadTileAtPath:(MKTileOverlayPath)path result:(void (^)(NSData *, NSError *))result {
-    NSMutableString *tileFilePath = [self.URLTemplate mutableCopy];
-    [tileFilePath replaceOccurrencesOfString: @"{x}" withString:[NSString stringWithFormat:@"%li", (long)path.x] options:0 range:NSMakeRange(0, tileFilePath.length)];
-    [tileFilePath replaceOccurrencesOfString:@"{y}" withString:[NSString stringWithFormat:@"%li", (long)path.y] options:0 range:NSMakeRange(0, tileFilePath.length)];
-    [tileFilePath replaceOccurrencesOfString:@"{z}" withString:[NSString stringWithFormat:@"%li", (long)path.z] options:0 range:NSMakeRange(0, tileFilePath.length)];
-    if ([[NSFileManager defaultManager] fileExistsAtPath:tileFilePath]) {
-        NSData* tile = [NSData dataWithContentsOfFile:tileFilePath];
-        result(tile,nil);
-    } else {
-        result(nil, nil);
-    }
-}*/
 
 
 @end
